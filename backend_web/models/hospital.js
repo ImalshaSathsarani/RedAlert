6ab6c07cf43891e1508ee9af2d74b94f499fccc5
Dewni <dewniandradi2021@gmail.com>
2025-06-28 21:35:06 +0530

    Add login,register,signout,change password functions

---
+++ backend_web/models/hospital.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const hospitalSchema = new mongoose.Schema(
+  {
+    hospitalName: { type: String, required: true },
+    type: { type: String, enum: ["Government", "Private"], required: true },
+    registrationNumber: { type: String, required: true, unique: true },
+    district: { type: String, required: true },
+    address: { type: String },
+
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    designation: { type: String, required: true },
+
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isApproved: { type: Boolean, default: false }, // Optional: approval status
+
+    registrationDocument: {
+      type: String, // File path or cloud URL (e.g., "/uploads/registrationForm.pdf")
+      required: true,
+    },
+    officeLetter: {
+      type: String, // File path or cloud URL
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Hospital", hospitalSchema);
